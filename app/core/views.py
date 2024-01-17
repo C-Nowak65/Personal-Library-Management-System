2123c7b2a612535bbb1fa83dc430e76d2f9be8a7
chno <ctnowak65@gmail.com>
2024-01-17 17:08:01 -0500

    [ADD/IMP] Added the front end functionality as well as some improvements to the backend. At this time am not going to push this to deployment but may revisit later.

--- app/core/views.py
+++ app/core/views.py
@@ -1,52 +1,54 @@
-from flask import render_template, redirect, url_for, flash
-from sqlalchemy import desc
-from .models import db, Books, User
-from .forms import BookForm, RegistrationForm, LoginForm, ProfileUpdateForm
-from . import app
+from flask import Blueprint, render_template, url_for, flash, redirect, current_app
+from .models import Books, User, db
+from app.core.forms import BookForm, RegistrationForm, LoginForm, ProfileUpdateForm
 from flask_login import login_user, login_required, current_user, logout_user
-from flask_bcrypt import Bcrypt
 
-bcrypt =  Bcrypt(app)
 
-@app.route('/add_book', methods=['GET', 'POST'])
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('index.html')
+
+@login_required
+@main.route('/add_book', methods=['GET', 'POST'])
 def add_book():
-    '''A Method to add books.'''
+    '''A Method to add a book.'''
     form = BookForm()
     if form.validate_on_submit():
-        app.logger.info(f"user_id: {current_user.user_id} adding book. ")
+        current_app.logger.info(f'{current_user.user_id} is registering a book.')
         try:
-            new_book = Books(title=form.title.data, author=form.author.data, genre=form.genre.data, start_date=form.start_date.data, end_date=form.end_date.data, status=form.status.data, rating=form.rating.data)
+            new_book = Books(title=form.title.data, author=form.author.data, genre=form.genre.data, start_date=form.start_date.data, end_date=form.end_date.data, status=form.status.data, rating=form.rating.data, user_id=current_user.user_id)
             db.session.add(new_book)
             db.session.commit()
-            flash('Book added successfully!')
-            app.logger.info(f"Book added successfully.")
+            flash('Book Registered Successfully!')
+            current_app.logger.info(f'{current_user.user_id} has registered a book successfully!')
         except Exception as e:
-            flash(f"Error fetching books: {e}")
-            app.logger.error(f"Error fetching books: {e}")
-        return redirect(url_for('index'))
+            flash('Error registering Books!')
+            current_app.logger.error(f'Error registering: {e}')
+        return redirect(url_for('main.my_books'))
     return render_template('add_book.html', form=form)
 
-
-@app.route('/register', methods=['GET', 'POST'])
+@main.route('/register', methods=['GET', 'POST'])
 def register():
     '''A Method to register a user.'''
     form = RegistrationForm()
     if form.validate_on_submit():
-        app.logger.info("New user attempting to register.")
+        current_app.logger.info("New user attempting to register.")
         try:
-            new_user = User(user_name=form.username.data)
+            new_user = User(user_name=form.username.data, user_password=form.password.data)
             new_user.set_password(form.password.data)
             db.session.add(new_user)
             db.session.commit()
             flash('User registered.')
-            app.logger.info("New user registered.")
+            current_app.logger.info("New user registered.")
         except Exception as e:
             flash(f"Error registering user: {e}")
-            app.logger.error(f"Error registering user: {e}")
-        return redirect(url_for('login'))
+            current_app.logger.error(f"Error registering user: {e}")
+        return redirect(url_for('main.login'))
     return render_template('register.html', form=form)
 
-@app.route('/login', methods=['GET', 'POST'])
+@main.route('/login', methods=['GET', 'POST'])
 def login():
     '''A method to login a user.'''
     form = LoginForm()
@@ -54,28 +56,28 @@ def login():
         user = User.query.filter_by(user_name=form.username.data).first()
         if user and user.check_password(form.password.data):
             login_user(user)
-            app.logger.info(f"User {user.user_name} successfully logged in.")
-            return redirect(url_for('index'))
+            current_app.logger.info(f"User {user.user_name} successfully logged in.")
+            return redirect(url_for('main.home'))
         else:
             flash("Invalid username or password.")
-            app.logger.info(f"Failed login attempt for username: {form.username.data}")
+            current_app.logger.info(f"Failed login attempt for username: {form.username.data}")
     return render_template('login.html', form=form)
 
-@app.route('/my_books')
+@main.route('/my_books')
 @login_required
 def my_books():
     '''A Method to show all of a users books.'''
     user_books = Books.query.filter_by(user_id=current_user.user_id).all()
-    return render_template('my_books.html', books=user_books)
+    return render_template('my_books.html', user_books=user_books)
 
-@app.route('/edit_book/<int:book_id>', methods=['GET', 'POST'])
+@main.route('/edit_book/<int:book_id>', methods=['GET', 'POST'])
 @login_required
 def edit_book(book_id):
     '''A method to allow users to edit their own books.'''
     book = Books.query.get_or_404(book_id)
     if book.user_id != current_user.user_id:
         flash('Book does not exist.')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.home'))
     
     form = BookForm(obj=book)
     if form.validate_on_submit():
@@ -88,24 +90,24 @@ def edit_book(book_id):
         book.rating = form.rating.data
         db.session.commit()
         flash('Book updated successfully!')
-        return redirect(url_for('my_books'))
+        return redirect(url_for('main.my_books'))
     
     return render_template('edit_book.html', form=form)
 
-@app.route('/all_books')
+@main.route('/all_books')
 def all_books():
     '''A method to show all books and which user added the book.'''
     books = Books.query.all()
     return render_template('all_books.html', books=books)
 
-@app.route('/logout')
+@main.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('You have been successfully logged out.')
-    return redirect(url_for('index'))
+    return redirect(url_for('main.home'))
 
-@app.route('/update_profile', methods=['GET', 'POST'])
+@main.route('/update_profile', methods=['GET', 'POST'])
 @login_required
 def update_profile():
     user = User.query.get(current_user.user_id)
@@ -117,7 +119,7 @@ def update_profile():
         user.set_password(form.password.data)
         db.session.commit()
         flash('Profile updated successfully!')
-        return redirect(url_for('index'))
+        return redirect(url_for('main.home'))
     
     return render_template('update_profile.html', form=form)
     
